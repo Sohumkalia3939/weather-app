3035850711688a03dca5038e8620352a6eb5dbb9
SohumKalia <kaliasohum1@gmail.com>
2021-03-11 14:50:37 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "839652ea6a69f5bc25c4ad52b7e414eb"
+    }
+}
+
+module.exports = constants;
